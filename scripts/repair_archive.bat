7b6d02eb540f7d9508e2d124ed363d9a5cb7aec9
Daniel Wong <drojfjord@gmail.com>
2019-07-13 23:02:13 +1000

    exit if any command failes in script files

--- scripts/repair_archive.bat
+++ scripts/repair_archive.bat
@@ -1,4 +1,5 @@
-7za x -aoa *.7z -otemp_extract_dir
-png_header_scanner temp_extract_dir
-7za a temp_result.7z ./temp_extract_dir/* -mx9 
-pause
+SETLOCAL EnableDelayedExpansion
+
+7za x -aoa *.7z -otemp_extract_dir || exit /b !ERRORLEVEL!
+png_header_scanner temp_extract_dir || exit /b !ERRORLEVEL!
+7za a temp_result.7z ./temp_extract_dir/* -mx9 || exit /b !ERRORLEVEL!
--- scripts/repair_archive.sh
+++ scripts/repair_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-7za x -aoa *.7z -omy_temp_extract_dir
-./png_header_scanner my_temp_extract_dir
-7za a temp_result.7z ./my_temp_extract_dir/* -mx9 
-rm -rf my_temp_extract_dir
+7za x -aoa *.7z -omy_temp_extract_dir || exit 1
+./png_header_scanner my_temp_extract_dir || exit 1
+7za a temp_result.7z ./my_temp_extract_dir/* -mx9 || exit 1
+rm -rf my_temp_extract_dir || exit 1
